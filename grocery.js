94e4939f572683f6b86c297c2672227e3dd4ef84
Pawan Solanki <pksaug2529@gmail.com>
2020-10-01 09:25:53 +0530

    Add files via upload

---
+++ grocery.js
@@ -0,0 +1,47 @@
+export const GROCERY =
+    [
+        {
+        id: 30,
+        name:'Tropicana Orange Juice',
+        image: '/assests/images/orange.jpeg',
+        category: 'liquids',
+        label:'Grocery',
+        price:150,
+        featured: true,
+        chosen:false,
+        description:'Fresh Oranges in every sip!'                    
+        },
+        {
+        id: 31,
+        name:'Tomato Ketchup',
+        image: '/assests/images/sauce.jpg',
+        category: 'Grocery',
+        label:'Tangy',
+        price:43,
+        featured: false,
+        chosen:false,
+        description:'Pulpy Tomatoes with wisk'
+        },
+        {
+        id: 32,
+        name:'Milk',
+        image: '/assests/images/milk.jpg',
+        category: 'Grocery',
+        label:'fresh',
+        price:30,
+        featured: false,
+        chosen:false,
+        description:'Protein and vitamins integrated!'
+        },
+        {
+        id: 33,
+        name:'Atta Shakti',
+        image: '/assests/images/aata.jpg',
+        category: 'Grocery',
+        label:'new',
+        price:444,
+        featured: false,
+        chosen:true,
+        description:'Whole Wheat Grains'
+        }
+    ];
